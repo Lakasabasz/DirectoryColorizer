0c0875465a45fd61d14833bb099694d6d47a2cd8
Lakasabasz <lakasabasz@gmail.com>
2024-03-03 23:36:16 +0100

    Init

---
+++ DirectoryColorizer/IconExtractor.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using SkiaSharp;
+
+namespace DirectoryColorizer;
+
+static class IconExtractor
+{
+    public static Icon? Extract(string file, int number, bool largeIcon)
+    {
+        ExtractIconEx(file, number, out var large, out var small, 1);
+        try
+        {
+            return Icon.FromHandle(largeIcon ? large : small);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+    
+    [DllImport("Shell32.dll", EntryPoint = "ExtractIconExW", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    private static extern int ExtractIconEx(string sFile, int iIndex, out IntPtr piLargeVersion, out IntPtr piSmallVersion, int amountIcons);
+}
---
+++ DirectoryColorizer/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DirectoryColorizer;
+using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+
+var icon = IconExtractor.Extract("C:\\WINDOWS\\system32\\imageres.dll", -3, true);
+if (icon is null)
+{
+    Console.WriteLine("Icon not found");
+    return;
+}
+
+var shift = 30f;
+
+var bitmapOriginal = icon.ToBitmap().ToSKBitmap();
+bitmapOriginal.Pixels = bitmapOriginal.Pixels.Select(color =>
+{
+    color.ToHsv(out var h, out var s, out var v);
+    return SKColor.FromHsv(Mod(h + shift, 360f), s, v, color.Alpha);
+}).ToArray();
+
+var bitmap = bitmapOriginal.ToBitmap();
+bitmap.Save("shifted.png");
+
+float Mod(float x, float m) => (x%m + m)%m;
